cdf56affbad48b55c214ebe6081d0583f8d5427c
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-04 10:06:41 +0900

    Initial Commit

---
+++ Sample2-FireStore/Sample2_FireStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample2_FireStoreApp.swift
+//  Sample2-FireStore
+//
+//  Created by keiji yamaki on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct Sample2_FireStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
